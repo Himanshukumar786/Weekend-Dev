b98ae6fb767a5fef4cc5ff85e37ec3f9fd0bb9d1
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-15 23:15:47 +0530

    Added Code

---
+++ sept15/diff_in_debugging.js
@@ -0,0 +1,12 @@
+function isEvenOrOdd(x, fn) {
+
+    // call fn here
+    fn();
+    
+    return x%2 == 0;
+}
+
+isEvenOrOdd(10, function () {
+    console.log("Processing....");
+    console.trace("Trace of the functions");
+});
