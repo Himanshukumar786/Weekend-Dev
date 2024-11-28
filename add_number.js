8622427be98d745c48614b3d923134522193ce03
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-28 23:14:08 +0530

    code

--- add_number.js
+++ add_number.js
@@ -1,5 +1,14 @@
-function add(a, b, c) {
-    return a+b+c;
+// added any number of arguments code 
+// 1. add(1, 2, 3, 4, 5) should return 15
+// 2. add(1, 2, 3, 4, 5, 6) should return 21
+// 3. add(1, 2, 3, 4, 5, 6, 7) should return 28
+
+function add() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
 }
 
-console.log(add(1,2,3));
+console.log(add(1, 2, 3, 4, 5)); // 15
