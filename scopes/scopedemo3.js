c73526736d0319386d63a4d50c0265032ecf96d0
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-12-23 18:41:19 +0530

    addec code

---
+++ scopes/scopedemo3.js
@@ -0,0 +1,17 @@
+var teacher = "sanket singh";
+
+function fun() {
+    var teacher = "Anurag";
+    content = "35";
+    console.log("hello", teacher);
+}
+
+function gun() {
+    var student = "karthik";
+    console.log("welcome to the class", student, "Teacher for your class is", teacher);
+}
+
+fun();
+gun();
+
+console.log(teacher, content);
