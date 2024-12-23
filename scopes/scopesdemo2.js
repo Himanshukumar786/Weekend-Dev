7ba37445734517516d04a73654a05e1a731b6c82
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-12-23 18:33:10 +0530

    added code

---
+++ scopes/scopesdemo2.js
@@ -0,0 +1,14 @@
+var teacher = "sanket singh";
+
+function fun() {
+    var teacher = "Anurag";
+    console.log("hello", teacher);
+}
+
+function gun() {
+    var student = "karthik";
+    console.log("welcome to the class", student, "Teacher for your class is", teacher);
+}
+
+fun();
+gun();
